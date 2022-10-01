737cae34811231991b0208e65bcc50de9c29774c
David Legendre <legendre2013@hotmail.com>
2022-09-30 23:51:24 -0500

    quitar awaits

--- src/Controllers/Users/user.controller.js
+++ src/Controllers/Users/user.controller.js
@@ -64,7 +64,7 @@ export const getDataUser = async (req, res) => {
 
 export const logout = async (req, res) =>{
   try {
-    let token = req.cookie
+    let token = req.cookie.token
     res.status(200).json({token})
   } catch (error) {
     console.error(error);
