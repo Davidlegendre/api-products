c65d7a397a9c4444c5436cd6435c0874716eb185
David Legendre <legendre2013@hotmail.com>
2022-10-01 00:02:38 -0500

    quitar awaits

--- src/Routes/user.routes.js
+++ src/Routes/user.routes.js
@@ -1,11 +1,9 @@
 import { Router } from "express";
-import { getDataUser, login, logout, register } from "../Controllers/Users/user.controller";
+import { getDataUser, login, register } from "../Controllers/Users/user.controller";
 import { ValidateLoginCampos, ValidateRegisterCampos, validateToken } from "../validators/user.validator";
 const router = Router()
 
 router.get("/getUser", validateToken, getDataUser)
 router.post("/register", ValidateRegisterCampos, register)
 router.post("/login", ValidateLoginCampos, login)
-router.post("/logout", logout)
-
 export default router
